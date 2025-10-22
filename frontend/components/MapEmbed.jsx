ef7abee8cdc0cf512377b76b39ef3b50b8ce6281
Lance-Tan <lance.tan@ufl.edu>
2025-10-22 11:19:01 -0400

    created .env folder

--- frontend/components/MapEmbed.jsx
+++ frontend/components/MapEmbed.jsx
@@ -1,5 +1,5 @@
 export default function MapEmbed({ query = "New York University" }) {
-  const apiKey = "AIzaSyCwTbcMCOCEtS2RdH9voFy5UCeOeU6RgMw";
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
   const src = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${encodeURIComponent(query)}`;
 
